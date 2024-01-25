6902a17c0201d67f2b3bac2bc7bfbb80f7be62f0
Toandev <105204332+toandvph19458@users.noreply.github.com>
2024-01-25 10:24:37 +0700

    update

--- src/components/TableBeautifull.jsx
+++ src/components/TableBeautifull.jsx
@@ -1,94 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-// fake data generator
+// Fake data generator
 const getItems = (count) =>
   Array.from({ length: count }, (v, k) => k).map((k) => ({
     id: `item-${k}`,
     content: `Item ${k}`,
   }));
 
-// a little function to help us with reordering the result
+// A little function to help us with reordering the result
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
-
   return result;
 };
 
-class TableBeautifull extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: getItems(10),
-    };
-    this.onDragEnd = this.onDragEnd.bind(this);
-  }
+const TableBeautiful = () => {
+  const [items, setItems] = useState(getItems(10));
 
-  onDragEnd(result) {
-    // dropped outside the list
+  const onDragEnd = (result) => {
+    // Dropped outside the list
     if (!result.destination) {
       return;
     }
 
-    const items = reorder(
-      this.state.items,
+    const updatedItems = reorder(
+      items,
       result.source.index,
       result.destination.index
     );
 
-    this.setState({
-      items,
-    });
-  }
+    setItems(updatedItems);
+  };
 
-  render() {
-    return (
-      <div style={{ padding: "2rem" }}>
-        <table>
-          <thead>
-            <tr>
-              <th>Item</th>
-              <th>Two</th>
-              <th>Three</th>
-              <th>Four</th>
-            </tr>
-          </thead>
+  return (
+    <div style={{ padding: "2rem" }}>
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Two</th>
+            <th>Three</th>
+            <th>Four</th>
+          </tr>
+        </thead>
 
-          <DragDropContext onDragEnd={this.onDragEnd}>
-            <Droppable droppableId="droppable">
-              {(provided) => (
-                <tbody {...provided.droppableProps} ref={provided.innerRef}>
-                  {this.state.items.map((item, index) => (
-                    <Draggable
-                      key={item.id}
-                      draggableId={item.id}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <tr
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          <td>{item.content}</td>
-                          <td>2</td>
-                          <td>3</td>
-                          <td>4</td>
-                        </tr>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </tbody>
-              )}
-            </Droppable>
-          </DragDropContext>
-        </table>
-      </div>
-    );
-  }
-}
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="droppable">
+            {(provided) => (
+              <tbody {...provided.droppableProps} ref={provided.innerRef}>
+                {items.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided) => (
+                      <tr
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <td>{item.content}</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                      </tr>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </tbody>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </table>
+    </div>
+  );
+};
 
-export default TableBeautifull;
+export default TableBeautiful;
